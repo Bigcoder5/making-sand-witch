f83bb49a0c538dd7ac5f49a206f49e1e85869871
Bigcoder5 <anirajan27@gmail.com>
2021-03-27 15:23:13 +0530

    Add files via upload

---
+++ sandwitch .js
@@ -0,0 +1,10 @@
+makingsandwitch;
+
+1.take the bread and tost it in the Pan
+2.take one egg broke it in the pan and spred the egg in the bread
+3.take some salt , pepper, turmeric powder and put in the egg
+4.take one onion and spred in the bread 
+5.take some cheese and stuffed into the bread
+
+
+     thatsolve............taaty sandwitch is ready
